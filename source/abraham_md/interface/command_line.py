8d949db796cc14e70e2e2639e27929194940acae
unkokaeru <unkokaeru@users.noreply.github.com>
2024-05-07 14:42:30 +0000

    @auto Formatting

--- source/abraham_md/interface/command_line.py
+++ source/abraham_md/interface/command_line.py
@@ -8,7 +8,7 @@ interface_logger = setup_logging()
 def command_line_interface() -> None:
     """
     Command line interface for the application.
-    
+
     Notes
     -----
     This function is the entry point for the command line interface.
